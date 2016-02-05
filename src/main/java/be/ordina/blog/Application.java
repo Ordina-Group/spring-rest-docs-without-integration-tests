615fcddeb802d9791d58ef5a4220a949b5e58bc7
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-02-05 23:20:07 +0100

    Setup of the application

---
+++ src/main/java/be/ordina/blog/Application.java
@@ -0,0 +1,14 @@
+package be.ordina.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
